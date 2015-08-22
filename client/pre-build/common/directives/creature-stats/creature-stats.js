65b9917f0cb4c867aa689dc08fde455e0ce8bb5b
celanajaya <petermsteele@gmail.com>
2015-08-22 18:02:46 -0400

    started working on login

---
+++ client/pre-build/common/directives/creature-stats/creature-stats.js
@@ -0,0 +1,7 @@
+app.directive('creatureStats', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'pre-build/common/directives/creature-stats/creature-stats.html',
+		controller: "PanelController"
+	};
+});
