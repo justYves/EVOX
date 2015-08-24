a2b4e9835489cba16e4f247fb1eac0a2547ed3a9
celanajaya <petermsteele@gmail.com>
2015-08-24 14:40:36 -0400

    fixing progress bar

--- client/pre-build/common/directives/progress-bar/progress-bar.js
+++
@@ -1,7 +0,0 @@
-app.directive('progressBar', function(){
-	return {
-		restrict: "E",
-		templateUrl: 'pre-build/common/directives/progress-bar/progress-bar.html',
-		controller: 'PanelController'
-	}
-});
