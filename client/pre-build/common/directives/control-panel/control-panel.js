4cfe4bce60bdb90ca267311accdbfb21a9dd2cbc
Yves Yuen <yves.j.yuen@gmail.com>
2015-08-24 23:17:04 -0400

    moved speed settings to control panel

--- client/pre-build/common/directives/control-panel/control-panel.js
+++ client/pre-build/common/directives/control-panel/control-panel.js
@@ -1,62 +1,131 @@
-app.directive('controlPanel', function(CameraFactory) {
+app.directive('controlPanel', function() {
 
-        return {
-            restrict: 'E',
-            scope: {},
-            templateUrl: "pre-build/common/directives/control-panel/control-panel.html",
-            controller: "PanelController"
-        };
+    return {
+      restrict: 'E',
+      scope: {},
+      templateUrl: "pre-build/common/directives/control-panel/control-panel.html",
+      controller: "PanelController"
+    };
 
-    })
-    .controller("PanelController", function($scope, AuthService, WorldsFactory, CreatureFactory, CameraFactory) {
-        AuthService.getLoggedInUser()
-            .then(function(user) {
-                $scope.user = user;
-            });
+  })
+  .controller("PanelController", function($scope, AuthService, WorldsFactory, CreatureFactory, CameraFactory, $q,$stateParams,$state) {
+    AuthService.getLoggedInUser()
+      .then(function(user) {
+        $scope.user = user;
+      });
 
-        $scope.world = WorldsFactory.getCurrentWorld();
-        $scope.points = 25;
-        $scope.stats = false;
+    $scope.world = WorldsFactory.getCurrentWorld();
+    $scope.points = 25;
+    $scope.stats = false;
 
-        $scope.getPercentages = function(creature) {
-            $scope.creature.healthPercentage = Math.round((creature.hp / creature.hpMax) * 100);
-            $scope.creature.hungerPercentage = Math.round((creature.hunger / creature.hpMax) * 100);
-        };
+    $scope.getPercentages = function(creature) {
+      $scope.creature.healthPercentage = Math.round((creature.hp / creature.hpMax) * 100);
+      $scope.creature.hungerPercentage = Math.round((creature.hunger / creature.hpMax) * 100);
+    };
 
-        $scope.statsShow = function() {
-            $scope.stats = !$scope.stats;
-        };
+    $scope.statsShow = function() {
+      $scope.stats = !$scope.stats;
+    };
 
-        $scope.killCreature = function() {
-            $scope.creature.die();
-            $scope.stats = false;
-        };
+    $scope.killCreature = function() {
+      $scope.creature.die();
+      $scope.stats = false;
+    };
 
-        // this function crashes browser!
-        $scope.procreate = function() {
-            $scope.creature.procreate();
-        };
+    // this function crashes browser!
+    $scope.procreate = function() {
+      $scope.creature.procreate();
+    };
 
-        $scope.fertilize = function() {
-            CameraFactory.setGrass(true);
-            console.log(game.map);
-        };
+    $scope.fertilize = function() {
+      CameraFactory.setGrass(true);
+      console.log(game.map);
+    };
 
-        $scope.$on("currentCreature", function(event, creature) {
-            $scope.stats = true;
-            $scope.creature = creature;
-            $scope.getPercentages($scope.creature);
-            $scope.$digest();
-        });
-
-
-        //fix you later, son...
-        // $scope.$watch(function() {return game.creatures},
-        //     function(newVal, oldVal) {
-        //         if (newVal !== oldVal) {
-        //             $scope.creatures = game.creatures;
-        //         }
-        //     });
-
-
+    $scope.$on("currentCreature", function(event, creature) {
+      $scope.stats = true;
+      $scope.creature = creature;
+      $scope.getPercentages($scope.creature);
+      $scope.$digest();
     });
+
+    $scope.play = function() {
+      game.play();
+    };
+    $scope.pause = function() {
+      game.pause();
+    };
+    $scope.speedUp = function() {
+      game.speedUp();
+    };
+    $scope.slowDown = function() {
+      game.slowDown();
+    };
+
+    $scope.save = function() {
+      var existing = [],
+        isNew = [];
+      game.creatures.forEach(function(creature) {
+        // deleting circular reference for JSON
+        delete creature.map;
+        delete creature.game;
+        delete creature.item;
+        if (creature._id) existing.push(creature);
+        else isNew.push(creature);
+      })
+      var allCreatures
+      updateCreatureStuff(existing)
+        .then(function() {
+          return postCreatureStuff(isNew)
+        })
+        .then(function(newCreatures) {
+          allCreatures = existing.concat(newCreatures);
+          return JSON.stringify(game.trees);
+        })
+        .then(function(trees) {
+          var updatedWorld = WorldsFactory.getCurrentWorld();
+          updatedWorld.map = game.map;
+          updatedWorld.trees = trees;
+          updatedWorld.creatures = allCreatures;
+
+          return WorldsFactory.updateWorld(updatedWorld);
+        })
+        .then(function() {
+          $state.go('worlds');
+        });
+    };
+
+
+    function updateCreatureStuff(arr) {
+        return $q.all(arr.map(function(creature) {
+            return CreatureFactory.updateCoord(creature.position)
+                .then(function() {
+                    return CreatureFactory.updateCoord(creature.rotation);
+                });
+        }));
+    }
+
+    function postCreatureStuff(arr) {
+        return $q.all(arr.map(function(creature) {
+            return CreatureFactory.postCoord([creature.position, creature.rotation])
+                .then(function(coords) {
+                    console.log('hello', coords, creature);
+                    creature.position = coords[0];
+                    creature.rotation = coords[1];
+                    console.log(creature);
+                    return CreatureFactory.postCreature(creature); //parents will be set here
+                });
+        }));
+    }
+
+
+    //fix you later, son...
+    // $scope.$watch(function() {return game.creatures},
+    //     function(newVal, oldVal) {
+    //         if (newVal !== oldVal) {
+    //             $scope.creatures = game.creatures;
+    //         }
+    //     });
+
+
+  });
--- client/pre-build/game/game.controller.js
+++ client/pre-build/game/game.controller.js
@@ -59,51 +59,6 @@ app.controller('GameController', function($scope, $stateParams, WorldsFactory, C
     target.setAttribute('data-y', y);
   });
 
-    $scope.play = function() {
-        game.play();
-    };
-    $scope.pause = function() {
-        game.pause();
-    };
-    $scope.speedUp = function() {
-        game.speedUp();
-    };
-    $scope.slowDown = function() {
-        game.slowDown();
-    };
-
-    $scope.save = function() {
-        var existing = [],
-            isNew = [];
-        game.creatures.forEach(function(creature) {
-            // deleting circular reference for JSON
-            delete creature.map;
-            delete creature.game;
-            delete creature.item;
-            if (creature._id) existing.push(creature);
-            else isNew.push(creature);
-        })
-        var allCreatures
-        updateCreatureStuff(existing)
-            .then(function() {
-                return postCreatureStuff(isNew)
-            })
-            .then(function(newCreatures) {
-                allCreatures = existing.concat(newCreatures);
-                return JSON.stringify(game.trees);
-            })
-            .then(function(trees) {
-                var updatedWorld = WorldsFactory.getCurrentWorld();
-                updatedWorld.map = game.map;
-                updatedWorld.trees = trees;
-                updatedWorld.creatures = allCreatures;
-
-                return WorldsFactory.updateWorld(updatedWorld);
-            })
-            .then(function() {
-                $state.go('worlds');
-            });
-    };
 
     function initMap() {
         map = MapFactory.getCurrentMap();
@@ -348,27 +303,6 @@ app.controller('GameController', function($scope, $stateParams, WorldsFactory, C
 
     // var start = window.start(game);
 
-    function updateCreatureStuff(arr) {
-        return $q.all(arr.map(function(creature) {
-            return CreatureFactory.updateCoord(creature.position)
-                .then(function() {
-                    return CreatureFactory.updateCoord(creature.rotation);
-                });
-        }));
-    }
-
-    function postCreatureStuff(arr) {
-        return $q.all(arr.map(function(creature) {
-            return CreatureFactory.postCoord([creature.position, creature.rotation])
-                .then(function(coords) {
-                    console.log('hello', coords, creature);
-                    creature.position = coords[0];
-                    creature.rotation = coords[1];
-                    console.log(creature);
-                    return CreatureFactory.postCreature(creature); //parents will be set here
-                });
-        }));
-    }
 
     // <------ PLAYER ------>
     // voxel-player: add player that can move around. It needs a copy of the game
