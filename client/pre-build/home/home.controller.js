7d1c9123d7315fe1564cb471aeb286959b55799c
celanajaya <petermsteele@gmail.com>
2015-08-25 16:09:04 -0400

    new landing page

--- client/pre-build/home/home.controller.js
+++ client/pre-build/home/home.controller.js
@@ -1,14 +1,4 @@
 app.controller('HomeController', function($scope, $http) {
-	// $(function() {
-	//     $('body').on('click', '.page-scroll a', function(event) {
-	//         var $anchor = $(this);
-	//         $('html, body').stop().animate({
-	//             scrollTop: $($anchor.attr('href')).offset().top
-	//         }, 1500, 'easeInOutExpo');
-	//         // event.preventDefault();
-	//     });
-	// });
-
 	$scope.toLogin = function() {
 		$state.go('login');
 	};
