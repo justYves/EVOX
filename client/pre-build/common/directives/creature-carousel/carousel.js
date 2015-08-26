07547251cbef86355285a9a60eaedb142250b775
Yves Yuen <yves.j.yuen@gmail.com>
2015-08-26 11:06:05 -0400

    added level for each creatures

--- client/pre-build/common/directives/creature-carousel/carousel.js
+++ client/pre-build/common/directives/creature-carousel/carousel.js
@@ -7,13 +7,15 @@ app.directive('creatureCarousel', function() {
             height: '=',
             width: '=',
             slides: '=',
-            file: '='
+            file: '=',
+            showLevel: '='
         },
         link: function(scope, elem, attr) {
             scope.creatures = [];
             for (var i = 0; i < scope.slides.length; i += 3) {
                 scope.creatures.push(scope.slides.slice(i, i + 3));
             };
+            scope.creatures = scope.creatures.reverse();
             scope.myInterval = 0;
             console.log(scope.creatures)
             scope.noWrapSlides = false;
