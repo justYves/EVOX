3246a66344add193e18c287cbed1d41d32caf560
Justin Kim <jkim430@gmail.com>
2015-08-27 11:05:28 -0400

    fixed creature loading

--- client/pre-build/game/game.state.js
+++ client/pre-build/game/game.state.js
@@ -2,6 +2,11 @@ app.config(function($stateProvider) {
     $stateProvider.state('game', {
         url: '/worlds/:id/play',
         templateUrl: '/pre-build/game/game.html',
-        controller: 'GameController'
+        controller: 'GameController',
+        resolve: {
+            user: function(UserFactory) {
+                return UserFactory.getUser(UserFactory.currentUser._id)
+            }
+        }
     });
 });
