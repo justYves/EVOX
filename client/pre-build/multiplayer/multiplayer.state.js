153152589ac70aa0af2bc9873768f78a65fcec8f
Justin Kim <jkim430@gmail.com>
2015-08-31 20:21:39 -0400

    multiplayer

--- client/pre-build/multiplayer/multiplayer.state.js
+++ client/pre-build/multiplayer/multiplayer.state.js
@@ -16,12 +16,4 @@ app.config(function($stateProvider) {
             authenticate: true
         }
     })
-        .state('multiplayer.waiting', {
-            url: '',
-            templateUrl: '/pre-build/multiplayer/multiplayer.waiting.html'
-        })
-        .state('multiplayer.play', {
-            templateUrl: '/pre-build/multiplayer/multiplayer.play.html',
-            controller: 'MultiplayerPlayController'
-        });
 });
